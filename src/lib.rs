a5948685105a282ff336b2141a17cd06ffba552b
Julian Gehring <juliangehring@users.noreply.github.com>
2019-02-26 23:30:16 +0000

    Split into library

---
+++ src/lib.rs
@@ -0,0 +1,115 @@
+use failure::Error;
+use hashbrown::HashMap;
+use rayon::slice::ParallelSliceMut;
+use signal_hook;
+use std::{
+    cmp::Ord,
+    fs::File,
+    io::{stdin, stdout, BufRead, BufReader, Write},
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+};
+use structopt::{
+    clap::{_clap_count_exprs, arg_enum},
+    StructOpt,
+};
+
+arg_enum! {
+    #[derive(Debug)]
+    pub enum SortingOrder {
+        Key,
+        Count,
+        None,
+    }
+}
+
+#[derive(StructOpt, Debug)]
+pub struct Config {
+    #[structopt(
+        long = "sortby",
+        short = "s",
+        default_value = "Count",
+        raw(
+            possible_values = "&SortingOrder::variants()",
+            case_insensitive = "true"
+        )
+    )]
+    pub sort_by: SortingOrder,
+    #[structopt(long = "top")]
+    pub top: Option<usize>,
+    #[structopt()]
+    pub input: Option<String>,
+}
+
+fn create_reader(input: &Option<String>) -> Result<Box<BufRead>, Error> {
+    let reader: Box<BufRead> = match input {
+        Some(file_name) => Box::new(BufReader::new(File::open(file_name)?)),
+        None => Box::new(BufReader::new(stdin())),
+    };
+    Ok(reader)
+}
+
+fn sort_counts<T: Ord + Sync>(counts: &mut Vec<(&String, &T)>, sorting_order: &SortingOrder) {
+    match sorting_order {
+        SortingOrder::Key => {
+            counts.par_sort_unstable_by(|a, b| a.0.cmp(b.0).then(a.1.cmp(b.1).reverse()))
+        }
+        SortingOrder::Count => {
+            counts.par_sort_unstable_by(|a, b| a.1.cmp(b.1).reverse().then(a.0.cmp(b.0)))
+        }
+        SortingOrder::None => (),
+    }
+}
+
+fn watch_sig_pipe() -> Result<Arc<AtomicBool>, Error> {
+    let sig_pipe = Arc::new(AtomicBool::new(false));
+    signal_hook::flag::register(signal_hook::SIGPIPE, Arc::clone(&sig_pipe))?;
+    Ok(sig_pipe)
+}
+
+fn count_items(reader: Box<BufRead>) -> Result<HashMap<std::string::String, u64>, Error> {
+    let mut counter: HashMap<_, u64> = Default::default();
+
+    for line in reader.lines() {
+        *counter.entry(line?).or_insert(0) += 1;
+    }
+
+    Ok(counter)
+}
+
+fn output_counts<T: Write>(
+    mut io: T,
+    counts: Vec<(&String, &u64)>,
+    n: usize,
+    sig_pipe: Arc<AtomicBool>,
+) -> Result<(), Error> {
+    for (key, count) in counts.iter().take(n) {
+        writeln!(io, "{}\t{}", key, count)?;
+        if sig_pipe.load(Ordering::Relaxed) {
+            break;
+        }
+    }
+    Ok(())
+}
+
+pub fn run(config: Config) -> Result<(), Error> {
+    let sig_pipe = watch_sig_pipe()?;
+
+    let reader = create_reader(&config.input)?;
+
+    let counter = count_items(reader)?;
+
+    let mut counts: Vec<_> = counter.iter().collect();
+    sort_counts(&mut counts, &config.sort_by);
+
+    let n = config.top.unwrap_or_else(|| counts.len());
+
+    let stdout = stdout();
+    let handle = stdout.lock();
+
+    output_counts(handle, counts, n, sig_pipe)?;
+
+    Ok(())
+}
--- tests/cli.rs
+++ tests/cli.rs
@@ -1,12 +1,13 @@
 use assert_cmd::prelude::{CommandCargoExt, OutputAssertExt};
+use failure::Error;
 use predicates::{
     prelude::PredicateBooleanExt,
     str::{contains, ends_with, starts_with},
 };
-use std::{error::Error, process::Command};
+use std::process::Command;
 
 #[test]
-fn test_toplevel_1k_counts() -> Result<(), Box<Error>> {
+fn test_toplevel_1k_counts() -> Result<(), Error> {
     let test_file = "tests/data/toplevel-1k.txt";
 
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
@@ -19,7 +20,7 @@ fn test_toplevel_1k_counts() -> Result<(), Box<Error>> {
 }
 
 #[test]
-fn test_toplevel_1k_counts_top5() -> Result<(), Box<Error>> {
+fn test_toplevel_1k_counts_top5() -> Result<(), Error> {
     let test_file = "tests/data/toplevel-1k.txt";
 
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
@@ -32,7 +33,7 @@ fn test_toplevel_1k_counts_top5() -> Result<(), Box<Error>> {
 }
 
 #[test]
-fn test_toplevel_1k_counts_sortby_count() -> Result<(), Box<Error>> {
+fn test_toplevel_1k_counts_sortby_count() -> Result<(), Error> {
     let test_file = "tests/data/toplevel-1k.txt";
 
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
@@ -49,7 +50,7 @@ fn test_toplevel_1k_counts_sortby_count() -> Result<(), Box<Error>> {
 }
 
 #[test]
-fn test_toplevel_1k_counts_sortby_key() -> Result<(), Box<Error>> {
+fn test_toplevel_1k_counts_sortby_key() -> Result<(), Error> {
     let test_file = "tests/data/toplevel-1k.txt";
 
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
@@ -62,7 +63,7 @@ fn test_toplevel_1k_counts_sortby_key() -> Result<(), Box<Error>> {
 }
 
 #[test]
-fn test_incorrect_file() -> Result<(), Box<Error>> {
+fn test_incorrect_file() -> Result<(), Error> {
     let test_file = "no_test_file_here";
 
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
